1cc72d45074de8b6cf43f9c0d4c6d13c20bb1d41
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-06-20 14:22:55 +0000

    salvar

---
+++ app/src/main/java/application/model/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package application.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+    
+    
+}
